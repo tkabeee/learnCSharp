abbcc802f707613acd4954b7161fbbae582641b0
tkabeee <tkabee@gmail.com>
2015-07-17 12:08:14 +0900

    デモサイトを作成

---
+++ demo/LogonForm.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LogonButton_Click(object sender, EventArgs e)
+    {
+        if (UserNameTextBox.Text == "test" &&
+            UserPasswordTextBox.Text == "test")
+        {
+            ResultMessageLabel.Text = "認証されました";
+            ResultMessageLabel.ForeColor = System.Drawing.Color.Blue;
+        }
+        else
+        {
+            ResultMessageLabel.Text = "認証できませんでした";
+            ResultMessageLabel.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}
